c483b7d8e1e82a145a2e66fd1bbf61b7562fdd71
slanto <slawek.antos@outlook.com>
2017-01-10 18:17:53 +0100

    Naive implementation

---
+++ FrogJmp/FrogJmp.Core/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace FrogJmp.Core
+{
+    public class Solution
+    {
+        public int GetFrogJump(int X, int Y, int D)
+        {
+            return 3; 
+        }
+    }
+}
---
+++ FrogJmp/FrogJmp.Tests/SolutionTests.cs
@@ -0,0 +1,17 @@
+﻿using FrogJmp.Core;
+using Xunit;
+
+namespace FrogJmp.Tests
+{   
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(10, 85, 30, 3)]
+        public void GetFrogJump(int x, int y, int step, int expected)
+        {
+            var sut = new Solution();
+            int actual = sut.GetFrogJump(x, y, step);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
